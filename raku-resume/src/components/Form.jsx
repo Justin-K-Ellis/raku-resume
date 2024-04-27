9c92cd98f4947a581ab1238a0e710f9698c79c72
Justin Klitgaard <jlke45@gmial.com>
2024-04-27 12:05:53 +0900

    Build form layout and functionality

--- raku-resume/src/components/Form.jsx
+++ raku-resume/src/components/Form.jsx
@@ -1,23 +1,48 @@
-const Form = () => {
+const Form = ({ handleSubmit, handleChange, userInfo }) => {
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      {/* <p>Full name: {userInfo.firstName + " " + userInfo.lastName}</p> */}
       <section className="general-info info-section">
         <h3>General Info</h3>
         <div className="form-section">
           <label htmlFor="first-name">First name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={userInfo.firstName}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-section">
           <label htmlFor="last-name">Last name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={userInfo.lastName}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-section">
           <label htmlFor="usr-email">Email</label>
-          <input type="email" id="usr-email" name="usr-email" />
+          <input
+            type="email"
+            id="usrEmail"
+            name="usrEmail"
+            value={userInfo.usrEmail}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-section">
           <label htmlFor="phone">Phone number</label>
-          <input type="text" id="phone" name="phone" />
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            value={userInfo.phone}
+            onChange={handleChange}
+          />
         </div>
       </section>
 
@@ -25,25 +50,61 @@ const Form = () => {
         <h3>Education</h3>
         <div className="form-section">
           <label htmlFor="ed">High level of education</label>
-          <input list="edu-types" id="ed" name="ed" />
+          <input
+            list="edu-types"
+            id="ed"
+            name="ed"
+            value={userInfo.ed}
+            onChange={handleChange}
+          />
           <datalist id="edu-types">
             <option value="PhD" />
             <option value="MA" />
-            <option value="BA, BS" />
+            <option value="BA" />
+            <option value="BS" />
             <option value="Highschool, GED" />
           </datalist>
         </div>
         <div className="form-section">
-          <label htmlFor="inst-name">Name of institution</label>
-          <input type="text" id="inst-name" name="inst-name" />
+          <label htmlFor="instName">Name of institution</label>
+          <input
+            type="text"
+            id="instName"
+            name="instName"
+            value={userInfo.instName}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-section">
-          <label htmlFor="grad-date">Graduation date</label>
-          <input type="date" id="grad-date" name="grad-date" />
+          <label htmlFor="major">Major/Specialization</label>
+          <input
+            type="text"
+            id="major"
+            name="major"
+            value={userInfo.major}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-section">
+          <label htmlFor="gradDate">Year of graduation</label>
+          <input
+            type="number"
+            id="gradDate"
+            name="gradDate"
+            value={userInfo.gradDate}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-section">
           <label htmlFor="certs">Certifications</label>
-          <textarea name="certs" id="certs" cols="30" rows="5"></textarea>
+          <textarea
+            name="certs"
+            id="certs"
+            cols="30"
+            rows="5"
+            value={userInfo.certs}
+            onChange={handleChange}
+          ></textarea>
         </div>
       </section>
 
@@ -51,24 +112,43 @@ const Form = () => {
         <h3>Most Recent Employment</h3>
         <div className="form-section">
           <label htmlFor="company">Company</label>
-          <input type="text" id="company" name="company" />
+          <input
+            type="text"
+            id="company"
+            name="company"
+            value={userInfo.company}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-section">
           <label htmlFor="position">Position</label>
-          <input type="text" id="position" name="position" />
+          <input
+            type="text"
+            id="position"
+            name="position"
+            onChange={handleChange}
+          />
         </div>
         <div className="form-section">
           <label htmlFor="resp">Key responsibilities</label>
-          <input type="text" id="resp" name="resp" />
+          <textarea
+            // type="text"
+            id="resp"
+            name="resp"
+            cols="30"
+            rows="5"
+            value={userInfo.resp}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-section">
           <label htmlFor="work-dates">Dates</label>
-          <textarea
-            name="work-dates"
-            id="work-dates"
-            cols="30"
-            rows="5"
-          ></textarea>
+          <input
+            name="workDates"
+            id="workDates"
+            value={userInfo.workDates}
+            onChange={handleChange}
+          ></input>
         </div>
       </section>
       <div className="btn-wrapper">
