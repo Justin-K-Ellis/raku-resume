1c9ca384d895fa64f99a1dbb6a1cc26b1d19ae39
Justin Klitgaard <jlke45@gmial.com>
2024-04-27 12:06:45 +0900

    Display description only on Form page
    The h2 and p telling the user what they can do now only displays on
    the Form render, and no longer with the Info component, where it is
    not necessary.

--- raku-resume/src/components/Main.jsx
+++ raku-resume/src/components/Main.jsx
@@ -1,12 +1,61 @@
+import { useState } from "react";
+
 import Form from "./Form";
+import Info from "./Info";
 
 const Main = () => {
+  const userDataTemplate = {
+    firstName: "",
+    lastName: "",
+    usrEmail: "",
+    phone: "",
+    ed: "",
+    instName: "",
+    major: "",
+    gradDate: "",
+    certs: "",
+    company: "",
+    position: "",
+    resp: "",
+    workDates: "",
+  };
+
+  const [userInfo, setUserInfo] = useState(userDataTemplate);
+  const [showInfo, setShowInfo] = useState(false);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setUserInfo((prevFormData) => ({ ...prevFormData, [name]: value }));
+  }
+
+  function handleSubmit(e) {
+    // e.preventDefaut();  => Todo: figure out what the issue is with this
+    // alert(showInfo);
+    let nextInfo = !showInfo;
+    setShowInfo(nextInfo);
+    // alert(nextInfo);
+  }
+
   return (
     <div className="outer-container shadow">
       <main>
-        <h2>Your Info</h2>
-        <p>Tell us about yourself and we'll craft the resume.</p>
-        <Form />
+        {showInfo ? (
+          <Info
+            userInfo={userInfo}
+            showInfo={showInfo}
+            setShowInfo={setShowInfo}
+          />
+        ) : (
+          <>
+            <h2>Your Info</h2>
+            <p>Tell us about yourself and we'll craft the resume.</p>
+            <Form
+              handleSubmit={handleSubmit}
+              handleChange={handleChange}
+              userInfo={userInfo}
+            />
+          </>
+        )}
       </main>
     </div>
   );
